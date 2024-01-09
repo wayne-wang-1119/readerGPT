6e6f35ed4f01cb28b6cd27e4cd8042b3786265b6
wayne-wang-1119 <haohanw@eecs.berkeley.edu>
2024-01-08 18:00:20 -0800

    update __init_.py

---
+++ agent/__init_.py
@@ -0,0 +1,2 @@
+# Use different agent for different classes. Should be moduler, which means we should define it to be simple enough.
+# Agent should do one thing: given a pdf, return a list of results/flags. The exact things to be ran by the agent should be moduler and changable.
